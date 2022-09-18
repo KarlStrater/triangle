ae6f910a5cc3715015b7db48454dbbf982c0b58a
blank <blank@DESKTOP-FIKQM1E>
2022-09-18 18:16:44 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿int n=0, s=0, ss=1; string bac=null; bool isNumber=false, rightone=false;
+while (!rightone)
+{
+    Console.WriteLine("Write the number:");
+    string? line = Console.ReadLine();
+    isNumber = int.TryParse(line, out n);
+    if (!isNumber)
+    {
+        Console.WriteLine("Wrong input... Try again)");
+    }
+    else if (n <= 0)
+    { Console.WriteLine("Wrong input... Try again)"); }
+    else if (n % 2 == 0)
+    { Console.WriteLine("Wrong input... Try again)"); }
+    else
+    {
+        rightone = true;
+    }
+}
+s = (n - 1)/2;
+Console.WriteLine(n);
+for (; ss <= n; ss+=2)
+{
+    for (int i = 0; i < s; i++)
+        bac += " ";
+    for (int j = 0; j < ss; j++)
+        bac += "*";
+    for(int i = 0; i < s; i++)
+        bac += " ";
+    Console.WriteLine(bac);
+    bac = null;
+    s --;
+}
+
+
